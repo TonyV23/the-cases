c0595d2fab6cab842c5a46efaf568f1d28006cd6
viper23 <htonnycarlos@gmail.com>
2023-05-10 07:07:53 +0200

    add feature for deleting a task

--- App.js
+++ App.js
@@ -1,46 +1,47 @@
-import { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-import Header from "./components/Header";
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
 
 export default function App() {
+    const [toDos, setToDos] = useState([
+        { text: 'buy coffee', key: '1' },
+        { text: 'create an app', key: '2' },
+        { text: 'go to the gym', key: '3' },
+        { text: 'play on the switch', key: '4' },
+        { text: 'workout', key: '5' }
+    ]);
 
-  const [toDos, setToDos] = useState([
-    { text: "buy coffee", key: "1" },
-    { text: "create an app", key: "2" },
-    { text: "go to the gym", key: "3" },
-    { text: "play on the switch", key: "4" },
-    { text: "workout", key: "5" },
-  ]);
+    const pressHandler = (key) => {
+        setToDos((prevTodos) => {
+            return prevTodos.filter((toDos) => toDos.key != key);
+        });
+    };
 
-  return (
-    <View style={styles.container}>
-      <View>
-        <Header/>
-        <View style={styles.content}>
-          {/* list */}
-          <View style={styles.list}>
-            <FlatList 
-              data={toDos}
-              renderItem={({item})=>(
-                <Text>{item.text}</Text>
-              )}
-            />
-          </View>
+    return (
+        <View style={styles.container}>
+            <View>
+                <Header />
+                <View style={styles.content}>
+                    {/* list */}
+                    <View style={styles.list}>
+                        <FlatList data={toDos} renderItem={({ item }) => <TodoItem item={item} pressHandler={pressHandler} />} />
+                    </View>
+                </View>
+            </View>
         </View>
-      </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  content :{
-    padding:40,
-  },
-  list :{
-    marginTop:20
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    content: {
+        padding: 40
+    },
+    list: {
+        marginTop: 20
+    }
 });
